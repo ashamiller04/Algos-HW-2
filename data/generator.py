d98a3a652ac9f51edfb367e4223a782314cb5a88
ashamiller04 <ashamiller@ufl.edu>
2026-03-06 18:55:03 -0500

    Basic File creation and Input generator setup

---
+++ data/generator.py
@@ -0,0 +1,41 @@
+#To Run: python generator.py [k] [m]
+# k = cache capacity, m = number of requests
+
+import random
+import sys
+
+#Validating Input
+try:
+    k = int(sys.argv[1])
+
+except:
+    print("Invalid Cache Capacity")
+    quit()
+
+if not (k>=1):
+    print("Cache Capacity cannot be less than 1")
+    quit()
+
+try:
+    m = int(sys.argv[2])
+
+except:
+    print("Invalid Number of Requests")
+    quit()
+
+
+#Initializing Request IDs
+ID_list = random.choices(range(0, 9999), k=m)
+
+list_length = len(ID_list)
+
+#Writing File
+with open(f"{k}_{m}.in", 'w') as f:
+    f.write(f"{k} {m}\n")
+    for ID in ID_list:
+        f.write(str(ID))
+        if (list_length > 1):
+            f.write(" ")
+        list_length -= 1
+
+#working! i think
